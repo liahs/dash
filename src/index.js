f171a23817d357d5d7daf0353ca8a8ba876b5999
liahs <liahschhetri2@gmail.com>
2020-09-23 10:50:52 +0530

    added dipunkuri

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
@@ -11,16 +11,54 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 
 import AdminLayout from "layouts/Admin.js";
 import Login from "views/login";
+import axios from 'axios'
+
 const hist = createBrowserHistory();
 
-ReactDOM.render(
-  <Login />
-  // <Router history={hist}>
-  //   <Switch>
-  //     <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-  //     <Redirect to="/admin/" />
-  //   </Switch>
-  // </Router>
-  ,
-  document.getElementById("root")
-);
+const transport = axios.create({
+  withCredentials: true,
+})
+
+export default function App() {
+  const [isauthenticated, authentication] = useState(false);
+  const [loading, changeloading] = useState(true);
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await transport.get('http://localhost:5000/admin/checksignin')
+      if(data.status){
+        changeloading(false)
+        authentication(true)
+      }
+      else{
+        changeloading(false)
+      }
+    }
+    fetchData()
+    },[isauthenticated])
+ const handleAuthentication=(x)=>{
+  authentication(x)
+  }
+      return (
+    <div>
+      {loading ? (
+        <h1 className="text-center  pt-5">Loading</h1>
+      ) : (
+        <Router history={hist}>
+          <Switch>
+            <Route exact path="/login">
+              <Login handleAuthentication={handleAuthentication} />
+            </Route>
+            <Route path="/admin" render={props => <AdminLayout {...props} />} />
+          </Switch>
+          {isauthenticated ? (
+            <Redirect to="/admin" />
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Router>
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/views/login.js
+++ src/views/login.js
@@ -1,82 +1,120 @@
 /* eslint-disable no-unused-vars */
-import React, { Component } from "react";
-import mainStyle from '../assets/css/main.css'
-import utilStyle from '../assets/css/util.css'
-import venStyle1 from '../assets/vendor/bootstrap/css/bootstrap.min.css'
-import venStyle2 from '../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'
-import venStyle3 from '../assets/vendor/animate/animate.css'
-import venStyle4 from '../assets/vendor/css-hamburgers/hamburgers.min.css'
-import venStyle5 from '../assets/vendor/select2/select2.min.css'
-import icon from '../assets/img/icons/favicon.ico'
-import Tilt from 'react-tilt';
+import React, { useState,useEffect } from "react";
+import mainStyle from "../assets/css/main.css";
+import utilStyle from "../assets/css/util.css";
+import venStyle1 from "../assets/vendor/bootstrap/css/bootstrap.min.css";
+import venStyle2 from "../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+import venStyle3 from "../assets/vendor/animate/animate.css";
+import venStyle4 from "../assets/vendor/css-hamburgers/hamburgers.min.css";
+import venStyle5 from "../assets/vendor/select2/select2.min.css";
+import icon from "../assets/img/icons/favicon.ico";
+import Tilt from "react-tilt";
+import axios from 'axios'
+const transport = axios.create({
+  withCredentials: true,
+})
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+function Login (props){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loader,setLoader]=useState(false)
+  const handleTextfield = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmail(e.target.value)
+        break
+      case "password":
+        setPassword(e.target.value)
+        break
     }
-
-    handleChange(event) {
-        console.log("Text Change Event", event)
-        // this.setState({ value: event.target.value });
+  }
+  const handleSubmit=async (e) => {
+    setLoader(true)
+    e.preventDefault()
+    const { data } = await transport.post('http://localhost:5000/admin/signin',{
+      email,
+      password
+    })
+    if(data.status){
+      props.handleAuthentication(true)
+      alert(data.exp)
     }
-
-    handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
-        console.log("Submit Event", event)
-        event.preventDefault();
+    else{
+      setLoader(false)
+      alert(data.exp)
     }
+  }
 
-    render() {
-        return (
-            <div className="limiter">
-                <div className="container-login100">
-                    <div className="wrap-login100">
-                        <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 250, width: 250 }} >
-                            <div className="login100-pic">
-                                <img src={require("../assets/img/assetPic.png")} alt="IMG" />
-                            </div>
-                        </Tilt>
-                        <form className="login100-form validate-form">
-                            <span className="login100-form-title">
-                                Admin Login
-                            </span>
+  return (
+      <div className="limiter">
+        <div className="container-login100">
+          <div className="wrap-login100">
+            <Tilt
+              className="Tilt"
+              options={{ max: 25 }}
+              style={{ height: 250, width: 250 }}
+            >
+              <div className="login100-pic js-tilt">
+                <img src={require("../assets/img/img-01.png")} alt="IMG" />
+              </div>
+            </Tilt>
+            <form
+              className="login100-form validate-form"
+              onSubmit={handleSubmit}
+            >
+              <span className="login100-form-title">Admin Login</span>
 
-                            <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                                <input className="input100" type="text" name="email" placeholder="Email" autoComplete="off"/>
-                                <span className="focus-input100"></span>
-                                <span className="symbol-input100">
-                                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                                </span>
-                            </div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Valid email is required: ex@abc.xyz"
+              >
+                <input
+                  className="input100"
+                  type="text"
+                  name="email"
+                  value={email}
+                  onChange={handleTextfield}
+                  placeholder="Email"
+                  autoComplete="off"
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i className="fa fa-envelope" aria-hidden="true"></i>
+                </span>
+              </div>
 
-                            <div className="wrap-input100 validate-input" data-validate="Password is required">
-                                <input className="input100" type="password" name="pass" placeholder="Password" />
-                                <span className="focus-input100"></span>
-                                <span className="symbol-input100">
-                                    <i className="fa fa-lock" aria-hidden="true"></i>
-                                </span>
-                            </div>
+              <div
+                className="wrap-input100 validate-input"
+                data-validate="Password is required"
+              >
+                <input
+                  className="input100"
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleTextfield}
+                  placeholder="Password"
+                />
+                <span className="focus-input100"></span>
+                <span className="symbol-input100">
+                  <i className="fa fa-lock" aria-hidden="true"></i>
+                </span>
+              </div>
 
-                            <div className="container-login100-form-btn">
-                                <button className="login100-form-btn">
-                                    Login
-                                </button>
-                            </div>
+              <div className="container-login100-form-btn">
+                <button className="login100-form-btn" type="submit">
+                {loader&&<span className="spinner-border spinner-border-sm mr-1" role="status" aria-hidden="true"></span>}
+                  Login
+                </button>
+              </div>
 
-                          
-                        </form>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+              <div className="text-center p-t-12"></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+
 }
 
 export default Login;
